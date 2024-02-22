59105dfc10d39eadf6eb0aa961dc3ae7ae0685ec
Alex <xrlogic1@gmail.com>
2024-02-22 21:21:56 +0400

    *

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+
+
 //
 // int intValue = 10;
 // object objValue = intValue; // boxing
@@ -21,11 +23,22 @@ using System.Collections;
 //
 // Console.WriteLine(sum);
 
+var myList = new MyList<int> { 10, 20, 30 };
+using var enumerator = myList.GetEnumerator();
+
+while (enumerator.MoveNext())
+{
+    Console.WriteLine(enumerator.Current);
+}
+
+enumerator.Reset();
+
 
 // BENCHMARK
 
 #region BENCHMARK
 
+#if _
 var capacity = 100_000_000;
 
 // List
@@ -63,4 +76,98 @@ var capacity = 100_000_000;
 
     Console.WriteLine($"non generic: {(DateTime.Now - begin).TotalMilliseconds}");
 }
+#endif
+
 #endregion
+
+interface IMyList<T>
+{
+    void Add(T value);
+    void Remove(T value);
+
+    int Length { get; }
+    T this[int index] { get; set; }
+}
+
+class MyList<T> : IMyList<T>, IEnumerable<T>
+{
+    private class MyListEnumerator : IEnumerator<T>
+    {
+        private readonly T[] _values;
+        private int _currentIndex;
+
+        public MyListEnumerator(T[] values)
+        {
+            _values = values;
+            _currentIndex = -1;
+        }
+
+        public bool MoveNext()
+        {
+            if (_currentIndex == _values.Length - 1)
+            {
+                return false;
+            }
+
+            _currentIndex += 1;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+
+        public T Current => _values[_currentIndex];
+
+        object IEnumerator.Current => Current!;
+
+        public void Dispose()
+        {
+        }
+    }
+
+    private T[] _values;
+
+    public MyList(params T[] @params)
+    {
+        _values = new T[@params.Length];
+
+        for (int i = 0; i < _values.Length; i++)
+        {
+            _values[i] = @params[i];
+        }
+
+        Length = @params.Length;
+    }
+
+    public void Add(T value)
+    {
+        Console.WriteLine("added");
+        // TODO: implement method
+    }
+
+    public void Remove(T value)
+    {
+        // TODO: implement method
+    }
+
+    public int Length { get; private set; }
+
+    public T this[int index]
+    {
+        get => _values[index];
+        set => _values[index] = value;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new MyListEnumerator(_values);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
