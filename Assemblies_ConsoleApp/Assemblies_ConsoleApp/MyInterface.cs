0dd533c87e8fe51a1a9fd5f02de4d153667ef46f
Alex <xrlogic1@gmail.com>
2024-03-12 21:03:20 +0400

    *

---
+++ Assemblies_ConsoleApp/Assemblies_ConsoleApp/MyInterface.cs
@@ -0,0 +1,23 @@
+﻿namespace Assemblies_ConsoleApp;
+
+interface IMyInterface
+{
+    void Foo();
+}
+
+
+internal class MyClass : IMyInterface
+{
+    public void Foo()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Test
+{
+    public static void Foo(MyClass obj)
+    {
+        
+    }
+}
